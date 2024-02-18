5975b6d77ea5f2634d1db076f3ac628344b68970
kimjunwoo <junwoo1027@naver.com>
2024-02-19 01:37:56 +0900

    제네릭과 무공변, 공변과 반공변

---
+++ src/main/kotlin/generic/Cage.kt
@@ -0,0 +1,32 @@
+package com.example.generic
+
+fun main() {
+    val goldFishCage = Cage<GoldFish>()
+    goldFishCage.put(GoldFish("금붕어"))
+
+    val cage = Cage<Fish>()
+    cage.moveFrom(goldFishCage)
+
+    goldFishCage.moveTo(cage)
+}
+
+// Generic Class
+class Cage<T> {
+    private val animals: MutableList<T> = mutableListOf()
+
+    fun getFirst(): T {
+        return animals.first()
+    }
+
+    fun put(animal: T) {
+        this.animals.add(animal)
+    }
+
+    fun moveFrom(otherCage: Cage<out T>) { // 생산자, 공변: 타입 파라미터의 상속 관계가 제네릭 클래스에서 유지된다.
+        this.animals.addAll(otherCage.animals)
+    }
+
+    fun moveTo(otherCage: Cage<in T>) { // 소비자, 반공변: 타입 파라미터의 상속관계가 제네릭 클래스에서 반대로 된다.
+        otherCage.animals.addAll(this.animals)
+    }
+}
