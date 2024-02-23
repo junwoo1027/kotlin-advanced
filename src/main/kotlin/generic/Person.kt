42f1f150878d278e46c6538927bf35f890732d19
kimjunwoo <junwoo1027@naver.com>
2024-02-23 23:32:01 +0900

    코틀린의 표준 위임 객체

--- src/main/kotlin/generic/Person.kt
+++ src/main/kotlin/generic/Person.kt
@@ -1,5 +1,7 @@
 package com.example.generic
 
+import kotlin.properties.Delegates
+
 class Person {
     lateinit var name: String // lateinit: 초기화를 지연시킨 변수, 초기화 로직이 여러 곳에 위치할 수 있다. 초기화 없이 호출하면 예외 발생
     val isKim: Boolean
@@ -14,4 +16,23 @@ class Person2 {
         Thread.sleep(2000)
         "김준우"
     }
+}
+
+class Person3 {
+    var age: Int by Delegates.observable(20) { _, oldValue, newValue ->
+        println("이전 값: $oldValue 새로운 값: $newValue")
+    }
+}
+
+class Person4 {
+    var age: Int by Delegates.vetoable(20) { _, _, newValue -> newValue >= 1}
+}
+
+fun main() {
+    val p = Person3()
+    p.age = 30
+
+    val p2 = Person4()
+    p2.age = 0
+    println(p2.age)
 }
