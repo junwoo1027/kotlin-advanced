ecb78c8947ba33c5c5eaf85fc7ff66cbaffd76b9
kimjunwoo <junwoo1027@naver.com>
2024-02-24 13:24:21 +0900

    고차함수와 함수 리터럴

---
+++ src/main/kotlin/function/Lec13.kt
@@ -0,0 +1,29 @@
+package com.example.function
+
+fun main() {
+    compute(1, 2) { a, b -> a + b }
+
+    compute(1, 2, fun(a, b) = a + b)
+}
+
+fun compute(num1: Int, num2: Int, op: (Int, Int) -> Int): Int {
+    return op(num1, num2)
+}
+
+fun calculate(num1: Int, num2: Int, oper: Operator) = oper.calcFun(num1, num2)
+
+enum class Operator(
+    private val oper: Char,
+    val calcFun: (Int, Int) -> Int
+) {
+    PLUS('+', { a, b -> a + b }),
+    MINUS('-', { a, b -> a - b }),
+    MULTIPLY('*', { a, b -> a * b }),
+    DIVIDE('/', { a, b ->
+        if (b == 0) {
+            throw IllegalArgumentException()
+        } else {
+            a / b
+        }
+    })
+}
