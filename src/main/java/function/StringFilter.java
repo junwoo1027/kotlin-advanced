7b2c46d0226b547a0d88e263c45857efb4e596d1
kimjunwoo <junwoo1027@naver.com>
2024-02-26 01:04:08 +0900

    SAM

---
+++ src/main/java/function/StringFilter.java
@@ -0,0 +1,5 @@
+package function;
+
+public interface StringFilter {
+    abstract boolean predicate(String str);
+}
---
+++ src/main/kotlin/function/Lec16.kt
@@ -0,0 +1,22 @@
+package com.example.function
+
+import function.StringFilter
+
+fun add(a: Int, b: Int): Int {
+    return a + b
+}
+
+fun main() {
+    // SAM 생성자
+    val filter = StringFilter { str -> str?.startsWith("A") ?: false }
+
+    val kStringFilter = KStringFilter { it.startsWith("*") }
+
+    val add2 = ::add
+
+    println(add2(1,2))
+}
+
+fun interface KStringFilter {
+    fun predicate(str: String): Boolean
+}
