5f92fbaa18ecd4d5f3f73e124c0cae3b37f91732
kimjunwoo <junwoo1027@naver.com>
2024-02-28 22:32:00 +0900

    코틀린의 어노테이션

---
+++ src/main/kotlin/reflection/Annotation.kt
@@ -0,0 +1,11 @@
+package com.example.reflection
+
+@Repeatable
+@Target(AnnotationTarget.FUNCTION, AnnotationTarget.FILE, AnnotationTarget.CLASS)
+annotation class Shape(
+    val texts: Array<String>
+)
+@Shape(["A", "C"])
+@Shape(["A", "B"])
+class Annotation {
+}
