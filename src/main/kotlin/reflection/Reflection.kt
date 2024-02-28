60b10a331513452f1fcbcdf81c1908662a07b4ad
kimjunwoo <junwoo1027@naver.com>
2024-02-28 23:07:50 +0900

    코틀린의 리플렉션

---
+++ src/main/kotlin/reflection/Reflection.kt
@@ -0,0 +1,40 @@
+package com.example.reflection
+
+import kotlin.reflect.KClass
+import kotlin.reflect.KFunction
+import kotlin.reflect.full.createType
+import kotlin.reflect.full.hasAnnotation
+
+@Target(AnnotationTarget.CLASS)
+annotation class Executable
+
+@Executable
+class Reflection {
+    fun a() {
+        println("A")
+    }
+
+    fun b(n: Int) {
+        println("B")
+    }
+}
+
+fun executeAll(obj: Any) {
+    val kClass: KClass<out Any> = obj::class
+
+    if (!kClass.hasAnnotation<Executable>()) {
+        return
+    }
+
+    val callableFunctions =  kClass.members.filterIsInstance<KFunction<*>>()
+        .filter { it.returnType == Unit::class.createType() }
+        .filter { it.parameters.size == 1 && it.parameters[0].type == kClass.createType() }
+
+    callableFunctions.forEach { function ->
+        function.call(obj)
+    }
+}
+
+fun main() {
+    executeAll(Reflection())
+}
